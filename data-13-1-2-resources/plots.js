09899dc744f1c945ca7e8854f3a7e9b5057ed029
Marco <marco_mireles@outlook.com>
2022-12-08 16:02:04 -0600

    included skill drills

--- data-13-1-2-resources/plots.js
+++ data-13-1-2-resources/plots.js
@@ -1,2 +1,6 @@
 // Created a chart with a single line of code 
+// Plotly.newPlot method creates a chart 
+    // There are 2 arguments 
+    // 'plotarea' corresponds to the ID of the <div> tag in the HTML doc
+    // The array is the plot points for the chart
 Plotly.newPlot("plotArea", [{x: [1, 2, 3], y: [10, 20, 30]}]);
