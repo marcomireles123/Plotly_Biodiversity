f0f6e1f91d426172f26040cfc7c6e5eb9bc7bd5a
Marco <marco_mireles@outlook.com>
2022-12-08 21:11:48 -0600

    dynamic charts

---
+++ data-13-4-3-resources/plots.js
@@ -0,0 +1,13 @@
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
