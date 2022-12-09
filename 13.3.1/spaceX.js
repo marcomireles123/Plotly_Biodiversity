8a3c5d768b3011f0a875181c55870340f3cbfef7
Marco <marco_mireles@outlook.com>
2022-12-08 19:55:06 -0600

    module work

--- 13.3.1/spaceX.js
+++ 13.3.1/spaceX.js
@@ -1,3 +1,8 @@
-const url = "https://api.spacexdata.com/v2/launchpads";
+// const url = "https://api.spacexdata.com/v2/launchpads";
 
-d3.json(url).then(receivedData => console.log(receivedData));
+// d3.json(url).then(receivedData => console.log(receivedData));
+
+const url = "https://api.spacexdata.com/v2/launchpads";
+d3.json(url).then(data => console.log(data));
+
+d3.json(url).then(spaceXResults => console.log(spaceXResults[0]));
