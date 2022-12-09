f7d03acde26b032c8429f15bc7dc270e43df0566
Marco <marco_mireles@outlook.com>
2022-12-08 21:05:36 -0600

    work

--- data-13-3-2-resources/plots.js
+++ data-13-3-2-resources/plots.js
@@ -24,3 +24,19 @@ d3.json("samples.json").then(function(data){
  null);
    console.log(filteredWfreq);
 });
+
+researcher1 = {
+   name: 'Roza',
+   age: 34,
+   hobby: 'Hiking'
+ };
+ console.log(Object.entries(researcher1));
+
+researcher1 = [['name', 'Roza'], ['age', 34], ['hobby', 'Hiking']];
+researcher1.forEach(([first, second]) => console.log(first + ": " + second));
+
+ d3.json("samples.json").then(function(data){
+   firstPerson = data.metadata[0];
+   Object.entries(firstPerson).forEach(([key, value]) =>
+     {console.log(key + ': ' + value);});
+ });
