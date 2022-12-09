7a1b073e170d0719f8973a0d2efd08406e49c3f2
Marco <marco_mireles@outlook.com>
2022-12-08 21:58:55 -0600

    deliverable 1

--- Starter_Code/charts.js
+++ Starter_Code/charts.js
@@ -100,7 +100,7 @@ function buildCharts(sample) {
     };
 
     // Deliverable 1: 10. Use Plotly to plot the data with the layout. 
-
+    Plotly.newPlot("bar", barData, barLayout);
     // Deliverable 2: 1. Create the trace for the bubble chart.
 
     // Deliverable 2: 2. Create the layout for the bubble chart.
