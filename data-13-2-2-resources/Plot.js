ba12b8a3681b1e5c1ea6a2629e917de5110fa9af
Marco <marco_mireles@outlook.com>
2022-12-08 19:04:41 -0600

    topFiveCityGrowths

--- data-13-2-2-resources/Plot.js
+++ data-13-2-2-resources/Plot.js
@@ -1,3 +1,6 @@
 var sortedCities = cityGrowths.sort((a,b) => a.Increase_from_2016 - b.Increase_from_2016).reverse(); 
+var topFiveCities = sortedCities.slice(0,5);
+var topFiveCityNames = cityGrowths.map(city => city.City);
+var topFiveCityGrowths = cityGrowths.map(city => parseInt(city.Increase_from_2016));
 
 console.log(cityGrowths);
