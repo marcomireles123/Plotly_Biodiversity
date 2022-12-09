d9e563e60cd8372a04c66a02460a7e610ca5c5b6
Marco <marco_mireles@outlook.com>
2022-12-08 20:10:07 -0600

    deleted null values

--- data-13-3-2-resources/plots.js
+++ data-13-3-2-resources/plots.js
@@ -16,3 +16,11 @@ d3.json("samples.json").then(function(data){
  person.wfreq).sort((a,b) => b - a);
    console.log(wfreq);
 });
+
+d3.json("samples.json").then(function(data){
+   wfreq = data.metadata.map(person =>
+ person.wfreq).sort((a,b) => b - a);
+   filteredWfreq = wfreq.filter(element => element !=
+ null);
+   console.log(filteredWfreq);
+});
